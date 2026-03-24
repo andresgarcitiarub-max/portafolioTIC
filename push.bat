df758e822d0aeceab0301e39445430e0e6d77296
Andres Garcia <andres.garcitiarub@unipaz.edu.co>
2026-03-24 10:33:11 -0500

    feat: dark/light theme toggle + Supabase contact form

--- push.bat
+++ push.bat
@@ -1,11 +1,5 @@
 @echo off
 set GIT="C:\Program Files\Git\cmd\git.exe"
-%GIT% config --global user.email "andres.garcitiarub@unipaz.edu.co"
-%GIT% config --global user.name "Andres Garcia"
-%GIT% init
 %GIT% add .
-%GIT% commit -m "Portafolio personal: Andres David Garcia Rubio"
-%GIT% branch -M main
-%GIT% remote remove origin 2>nul
-%GIT% remote add origin https://github.com/andresgarcitiarub-max/portafolioTIC.git
-%GIT% push -u origin main
+%GIT% commit -m "feat: dark/light theme toggle + Supabase contact form"
+%GIT% push origin main
